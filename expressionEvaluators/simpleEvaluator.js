db294441d3b8b09a3545be5027345d7594cf71f7
Julian Haeger <julianhaeger@gmail.com>
2012-12-11 17:07:12 +0000

    SimpleEvaluator has crude support for + - and * binary operators

--- expressionEvaluators/simpleEvaluator.js
+++ expressionEvaluators/simpleEvaluator.js
@@ -1,14 +1,75 @@
 var SimpleEvaluator = (function(){
+	
+	var shouldEvaluateAsExpression = function(input){
+		return typeof(input) === 'string' && input[0] === '=';
+	}
+
 	return function(){
 		var self = this;
 		
+		//Private
+		var evaluateExpression = function(expression){
+			var operators = [
+				
+				{op:'+',
+				func:function(terms){
+					var total = 0;
+					for (var i = 0; i < terms.length; i++) {
+						var term = terms[i];
+						total += evaluateExpression(term);
+					};
+					return total;
+				}},
+
+				{op:'-',
+				func:function(terms){
+					var total = evaluateExpression(terms[0]);
+					for (var i = 1; i < terms.length; i++) {
+						var term = terms[i];
+						total -= evaluateExpression(term);
+					};
+					return total;
+				}},
+
+				{op:'*',
+				func:function(terms){
+					var total = evaluateExpression(terms[0]);
+					for (var i = 1; i < terms.length; i++) {
+						var term = terms[i];
+						total *= evaluateExpression(term);
+					};
+					return total;
+				}}
+			];
+
+			//Find first operator which appears
+			for (var i = 0; i < operators.length; i++) {
+				var operator = operators[i];
+				var pieces = expression.split(operator.op);
+				if(pieces.length > 1)
+				{
+					//apply operator
+					return operator.func(pieces);
+				}
+
+			};
+
+			//No operators found, treat as plain value
+			return parseInt(expression);					
+		}
+
+		//Public
 		self.evaluate = function(expression){
-			if(expression[0] === '=')
+			if(shouldEvaluateAsExpression(expression))
 			{
-				return 0;
+				//remove the leading equals
+				expression = expression.slice(1);
+				return evaluateExpression(expression);
 			}
 			
 			return expression;
 		}
+
+		
 	}
 })();
--- tests/Evaluator-Tests/SimpleEvaluatorTests.js
+++ tests/Evaluator-Tests/SimpleEvaluatorTests.js
@@ -6,3 +6,43 @@ test( "Given input which does not start with '=', returns input without modifica
 	equal(evaluator.evaluate("hello"), "hello");
 	equal(evaluator.evaluate(1), 1);
 });
+
+test("Given an addition expression with two interger terms, returns their sum", function(){
+	var evaluator = new SimpleEvaluator();
+
+	var expression = "=2+3";
+
+	equal(evaluator.evaluate(expression), 5);
+})
+
+test("Given an addition expression with multiple interger terms, returns their sum", function(){
+	var evaluator = new SimpleEvaluator();
+
+	var expression = "=2+3+8";
+
+	equal(evaluator.evaluate(expression), 13);
+})
+
+test("Given a subtraction expression with two interger terms, returns the correct result", function(){
+	var evaluator = new SimpleEvaluator();
+
+	var expression = "=5-3";
+
+	equal(evaluator.evaluate(expression), 2);
+})
+
+test("Given an expression containing additions and subtractions, returns the correct result", function(){
+	var evaluator = new SimpleEvaluator();
+
+	var expression = "=5-3+33-8";
+
+	equal(evaluator.evaluate(expression), 27);
+})
+
+test("Given an expression containing additions, subtractions and multiplications, returns the correct result", function(){
+	var evaluator = new SimpleEvaluator();
+
+	var expression = "=5*2-8*3+10";
+
+	equal(evaluator.evaluate(expression), -4);
+})
