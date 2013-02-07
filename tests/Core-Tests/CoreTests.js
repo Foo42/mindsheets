87e9f52ddba975e6e43e28a3f0dbd528f4a9c143
OpenShift guest <julianhaeger@gmail.com>
2013-02-07 08:52:52 -0500

    Tests now working under require.js

--- tests/Core-Tests/CoreTests.js
+++ tests/Core-Tests/CoreTests.js
@@ -1,159 +1,161 @@
-
-module("SingleValueSource Tests")
-test("can create SingleValueSource", function() {
-	var svs = new SingleValueSource();
-	ok( svs != null);
-})
-
-test("SingleValueSource has default value of null", function(){
-	var svs = new SingleValueSource();
-	strictEqual(svs.Value(), null);
-})
-
-test("Can set and retrieve value on a SingleValueSource", function(){
-	var svs = new SingleValueSource();
-	svs.Value(4);
-	strictEqual(svs.Value(), 4);
-})
-
-test("Setting the value of one SingleValueSource does not change the value of others", function(){
-	var a = new SingleValueSource();
-	var b = new SingleValueSource();
-
-	a.Value(3);
-	b.Value(5);
-
-	equal(a.Value(), 3);
-	equal(b.Value(), 5);
-})
-
-test("setting a value on a SingleValueSource raises value change event with the new value", function(){
-	var svs = new SingleValueSource();
-	var eventFired = false;
-	var valueFromEvent;
-	svs.bind("valueChanged", function(newValue){
-		eventFired = true;
-		valueFromEvent = newValue;
-	});
-
-	svs.Value(4);
-
-	ok(eventFired);
-	strictEqual(valueFromEvent, 4);
-})
-
-test("setting the definition on a SingleValueSource raises definitionChange event with the new definition", function(){
-	var svs = new SingleValueSource();
-	var eventFired = false;
-	var valueFromEvent;
-
-	svs.bind("definitionChanged", function(newDefinition){
-		eventFired = true;
-		valueFromEvent = newDefinition;
-	});
-
-	svs.Definition(5);
-
-	ok(eventFired);
-	equal(valueFromEvent, 5);
-})
-
-test("setting the definition on a SingleValueSource triggers a call to its evaluator", function(){
-	var evaluatorWasCalled = false;
-	var evaluator = {evaluate:function(){evaluatorWasCalled=true;}};
-	var svs = new SingleValueSource(evaluator);
-
-	svs.Definition(3);
-
-	ok(evaluatorWasCalled);
-})
-
-test("setting a definition on a SingleValueSource which causes iggers a valueChanged event with the new value", function(){
-	var evaluator = {evaluate:function(expression){return expression + 1;}};
-	var svs = new SingleValueSource(evaluator);
-
-	var eventWasFired = false;
-	var valueFromEvent;
-
-	svs.bind("valueChanged", function(newValue){
-		valueFromEvent = newValue;
-		eventWasFired = true;
-	});
-
-	svs.Definition(2);
-
-	ok(eventWasFired);
-	equal(valueFromEvent, 3);
-})
-
-test("setting the definition on a SingleValueSource causes the value to change to the result from the evaluator", function(){
-	var evaluator = {evaluate:function(expression){return expression + 1;}};
-	var svs = new SingleValueSource(evaluator);
-
-	svs.Definition(2);
-	equal(3, svs.Value());
-})
-
-
-
-module("Sheet Tests")
-test("Adding an item to the sheet raises an itemAdded event on the sheet with the new item as an event argument", function(){
-	var itemToAdd = new SheetElement(new SingleValueSource(), {x:0,y:0});
-
-	var sheet = new Sheet();
-
-	var eventFired = false;
-	var itemInEvent = null;
-	sheet.bind("itemAdded", function(item){
-		eventFired = true;
-		itemInEvent = item;
-	});
-
-	sheet.addItem(itemToAdd);
-	
-	ok(eventFired);
-	strictEqual(itemInEvent, itemToAdd);
-})
-
-test("Sheet raises a nameAssigned event when trySetName is called for an item which was not previously named", function(){
-	var itemToAdd = new SheetElement(new SingleValueSource(), {x:0,y:0});
-	var sheet = new Sheet();
-	sheet.addItem(itemToAdd);
-
-	var eventFired = false;
-	var recordInEvent = null;
-	
-	sheet.bind("nameAssigned", function(record){
-		eventFired = true;
-		recordInEvent = record;
-	});
-
-	sheet.trySetName(itemToAdd, "Foo");
-	
-	ok(eventFired);
-	notStrictEqual(recordInEvent, {item:itemToAdd, name:"Foo"});	
-});
-
-test("Sheet raises a nameAssigned event with new name, when trySetName is called for an item which already had a name before", function(){
-	var itemToAdd = new SheetElement(new SingleValueSource(), {x:0,y:0});
-	var sheet = new Sheet();
-	sheet.addItem(itemToAdd);
-	var originalName = "Foo";
-	sheet.trySetName(itemToAdd, "Foo");
-
-	var eventFired = false;
-	var recordInEvent = null;
-	
-	sheet.bind("nameAssigned", function(record){
-		eventFired = true;
-		recordInEvent = record;
-	});
-
-	//Act
-	var newerName = "Bar";
-	sheet.trySetName(itemToAdd, newerName);
-	
-	//Assert
-	ok(eventFired);
-	notStrictEqual(recordInEvent, {item:itemToAdd, name:newerName});	
+define(['core/core'],function(core){
+    
+    module("SingleValueSource Tests")
+    test("can create SingleValueSource", function() {
+    	var svs = new core.SingleValueSource();
+    	ok( svs != null);
+    })
+    
+    test("SingleValueSource has default value of null", function(){
+    	var svs = new core.SingleValueSource();
+    	strictEqual(svs.Value(), null);
+    })
+    
+    test("Can set and retrieve value on a SingleValueSource", function(){
+    	var svs = new core.SingleValueSource();
+    	svs.Value(4);
+    	strictEqual(svs.Value(), 4);
+    })
+    
+    test("Setting the value of one SingleValueSource does not change the value of others", function(){
+    	var a = new core.SingleValueSource();
+    	var b = new core.SingleValueSource();
+    
+    	a.Value(3);
+    	b.Value(5);
+    
+    	equal(a.Value(), 3);
+    	equal(b.Value(), 5);
+    })
+    
+    test("setting a value on a SingleValueSource raises value change event with the new value", function(){
+    	var svs = new core.SingleValueSource();
+    	var eventFired = false;
+    	var valueFromEvent;
+    	svs.bind("valueChanged", function(newValue){
+    		eventFired = true;
+    		valueFromEvent = newValue;
+    	});
+    
+    	svs.Value(4);
+    
+    	ok(eventFired);
+    	strictEqual(valueFromEvent, 4);
+    })
+    
+    test("setting the definition on a SingleValueSource raises definitionChange event with the new definition", function(){
+    	var svs = new core.SingleValueSource();
+    	var eventFired = false;
+    	var valueFromEvent;
+    
+    	svs.bind("definitionChanged", function(newDefinition){
+    		eventFired = true;
+    		valueFromEvent = newDefinition;
+    	});
+    
+    	svs.Definition(5);
+    
+    	ok(eventFired);
+    	equal(valueFromEvent, 5);
+    })
+    
+    test("setting the definition on a SingleValueSource triggers a call to its evaluator", function(){
+    	var evaluatorWasCalled = false;
+    	var evaluator = {evaluate:function(){evaluatorWasCalled=true;}};
+    	var svs = new core.SingleValueSource(evaluator);
+    
+    	svs.Definition(3);
+    
+    	ok(evaluatorWasCalled);
+    })
+    
+    test("setting a definition on a SingleValueSource which causes iggers a valueChanged event with the new value", function(){
+    	var evaluator = {evaluate:function(expression){return expression + 1;}};
+    	var svs = new core.SingleValueSource(evaluator);
+    
+    	var eventWasFired = false;
+    	var valueFromEvent;
+    
+    	svs.bind("valueChanged", function(newValue){
+    		valueFromEvent = newValue;
+    		eventWasFired = true;
+    	});
+    
+    	svs.Definition(2);
+    
+    	ok(eventWasFired);
+    	equal(valueFromEvent, 3);
+    })
+    
+    test("setting the definition on a SingleValueSource causes the value to change to the result from the evaluator", function(){
+    	var evaluator = {evaluate:function(expression){return expression + 1;}};
+    	var svs = new core.SingleValueSource(evaluator);
+    
+    	svs.Definition(2);
+    	equal(3, svs.Value());
+    })
+    
+    
+    
+    module("Sheet Tests")
+    test("Adding an item to the sheet raises an itemAdded event on the sheet with the new item as an event argument", function(){
+    	var itemToAdd = new core.SheetElement(new core.SingleValueSource(), {x:0,y:0});
+    
+    	var sheet = new core.Sheet();
+    
+    	var eventFired = false;
+    	var itemInEvent = null;
+    	sheet.bind("itemAdded", function(item){
+    		eventFired = true;
+    		itemInEvent = item;
+    	});
+    
+    	sheet.addItem(itemToAdd);
+    	
+    	ok(eventFired);
+    	strictEqual(itemInEvent, itemToAdd);
+    })
+    
+    test("Sheet raises a nameAssigned event when trySetName is called for an item which was not previously named", function(){
+    	var itemToAdd = new core.SheetElement(new core.SingleValueSource(), {x:0,y:0});
+    	var sheet = new core.Sheet();
+    	sheet.addItem(itemToAdd);
+    
+    	var eventFired = false;
+    	var recordInEvent = null;
+    	
+    	sheet.bind("nameAssigned", function(record){
+    		eventFired = true;
+    		recordInEvent = record;
+    	});
+    
+    	sheet.trySetName(itemToAdd, "Foo");
+    	
+    	ok(eventFired);
+    	notStrictEqual(recordInEvent, {item:itemToAdd, name:"Foo"});	
+    });
+    
+    test("Sheet raises a nameAssigned event with new name, when trySetName is called for an item which already had a name before", function(){
+    	var itemToAdd = new core.SheetElement(new core.SingleValueSource(), {x:0,y:0});
+    	var sheet = new core.Sheet();
+    	sheet.addItem(itemToAdd);
+    	var originalName = "Foo";
+    	sheet.trySetName(itemToAdd, "Foo");
+    
+    	var eventFired = false;
+    	var recordInEvent = null;
+    	
+    	sheet.bind("nameAssigned", function(record){
+    		eventFired = true;
+    		recordInEvent = record;
+    	});
+    
+    	//Act
+    	var newerName = "Bar";
+    	sheet.trySetName(itemToAdd, newerName);
+    	
+    	//Assert
+    	ok(eventFired);
+    	notStrictEqual(recordInEvent, {item:itemToAdd, name:newerName});	
+    });
 });
---
+++ tests/ViewModel-Tests/ViewModelTests.js
@@ -0,0 +1,124 @@
+define(['core/core', 'ui/ui'],function(core, ui){
+    
+    var getSheetForTests = function(){
+        return new core.Sheet();
+    }
+    
+    
+    module("SingleValueViewModel Tests");
+    test("SingleValueViewModel updates its value when its SingleValueSource changes its value", function() {
+        //Arrange
+        var svs = new core.SingleValueSource();
+    	svs.Value(1);
+    	var vm = new ui.SingleValueViewModel({valueSource:svs}, getSheetForTests());
+    	var viewModelRaisedEvent = false;
+    	vm.value.subscribe(function(){viewModelRaisedEvent = true});
+    
+    
+    	//Act
+    	svs.Value(2);
+    
+    	//Assert
+    	ok(viewModelRaisedEvent);
+    	equal(vm.value(), 2);
+    })
+    
+    test("Changing the definition on a SingleValueViewModel updates the definition on its SingleValueSource", function() {
+    	//Arrange
+    	var svs = new core.SingleValueSource();
+    	svs.Definition("0");
+    	var vm = new ui.SingleValueViewModel({valueSource:svs}, getSheetForTests());
+    
+    	//Act
+    	vm.definition("1");
+    
+    	//Assert
+    	equal(svs.Definition(), "1");
+    })
+    
+    test("Setting requestedName on a SingleValueViewModel causes it to call trySetName on the sheet it was constructed with, ",function(){
+    	//Arrange
+    	var itemToAdd = new core.SheetElement(new core.SingleValueSource(), {x:0,y:0});
+    	
+    	var functionWasCalled = false;
+    	var itemPassedInFunction;
+    	var nameRequestedInFunction;
+    	var sheet = {
+            trySetName:function(item, requestedName){
+    		    functionWasCalled=true;
+    		    itemPassedInFunction = item;
+    		    nameRequestedInFunction = requestedName;
+    	    },
+            bind:function(){}
+        };
+    
+    	var vm = new ui.SingleValueViewModel(itemToAdd, sheet);
+    
+    	//Act
+    	vm.requestedName("Foo");
+    
+    	//Assert
+    	ok(functionWasCalled);
+    	strictEqual(itemPassedInFunction, itemToAdd);
+    	strictEqual(nameRequestedInFunction, "Foo");
+    });
+    
+    test("SingleValueViewModel name property takes its value from name assigment on sheet", function(){
+    	//Arrange
+    	var sheetModel = new core.Sheet();
+    	var sheetVM = new ui.SheetVM(sheetModel);
+    
+    	var item = new core.SheetElement(new core.SingleValueSource(), {x:0,y:0})
+    	sheetModel.addItem(item);
+    
+    	var itemVM = sheetVM.items()[0];
+    	equal(itemVM.name(), undefined);
+    
+    	//Act
+    	var chosenName = "foo";
+    	sheetModel.trySetName(item, chosenName);
+    
+    	//Assert	
+    	equal(itemVM.name(), chosenName);
+    });
+    
+    module("SheetViewModel Tests");
+    test("When the Sheet model adds an item, the SheetViewModel adds a corresponding ItemViewModel", function(){
+    	//Arrange
+    	var sheetModel = new core.Sheet();
+    	var sheetVM = new ui.SheetVM(sheetModel);
+    	var vmRaisedEvent = false;
+    	sheetVM.items.subscribe(function(){vmRaisedEvent = true});
+    	equal(sheetVM.items().length, 0);
+    
+    	var itemToAdd = new core.SingleValueSource();
+    
+    	//Act
+    	sheetModel.addItem({valueSource:itemToAdd});
+    
+    	//Assert
+    	ok(vmRaisedEvent);
+    	equal(sheetVM.items().length, 1);
+    });
+    
+    test("When sheet model assigns a name, view model for that item changes its name", function(){
+    	//Arrange
+    	var sheet = new core.Sheet();
+    	var item = new core.SheetElement(new core.SingleValueSource(), {x:0,y:0});
+    	
+    	var sheetVM = new ui.SheetVM(sheet);
+        
+    	sheet.addItem(item);
+        var itemVM = sheetVM.items()[0];
+    
+    	//Act
+    	sheet.trySetName(item, "Foo");
+    
+    
+    	//Assert
+    	equal(itemVM.name(), "Foo");
+    });    
+});
+
+
+
